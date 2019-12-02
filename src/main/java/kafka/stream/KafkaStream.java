f4b999db1ea61df58e6887123b73bf1cee884845
Rafael Bernabeu <rbbernabeu@gmail.com>
2019-12-01 22:45:54 -0300

    add producer

--- src/main/java/kafka/stream/KafkaStream.java
+++ src/main/java/kafka/stream/KafkaStream.java
@@ -1,20 +1,32 @@
 package kafka.stream;
 
+import io.reactivex.Flowable;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class KafkaStream {
 
+    private Random random = new Random();
+
+    @Broadcast
     @Incoming("entrada")
     @Outgoing("saida")
-    @Broadcast
     public String process(String data) {
         System.out.println(data);
         return data + " passou pelo quarkus";
+    }
+
+    @Broadcast
+    @Outgoing("test")
+    public Flowable<Integer> generate() {
+        return Flowable.interval(1, TimeUnit.SECONDS)
+                .map(tick -> random.nextInt(100));
     }
 
 }
